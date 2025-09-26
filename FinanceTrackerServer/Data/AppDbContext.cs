d1a07bc14d337a1203030c1ee2695512a2f06f3c
User <valera.astroukh@gmail.com>
2025-09-26 07:47:50 +0300

    AppDbContext created

---
+++ FinanceTrackerServer/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using FinanceTrackerServer.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTrackerServer.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка отношений
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Group)
+                .WithMany(g => g.Users)
+                .HasForeignKey(u => u.GroupId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Transactions)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Transaction>()
+               .HasOne(t => t.Group)
+               .WithMany(g => g.Transactions)
+               .HasForeignKey(t => t.GroupId)
+               .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
